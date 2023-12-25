ed24bf7541667176a1938deb7178ed8971e00bf9
zsliu98 <zsliu98@gmail.com>
2023-12-24 20:46:51 -0500

    ci: configure boost

---
+++ source/dsp/compressor/computer/knee_computer.cpp
@@ -0,0 +1,69 @@
+// ==============================================================================
+// Copyright (C) 2023 - zsliu98
+// This file is part of ZLEComp
+//
+// ZLEComp is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// ZLEComp is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with ZLEComp. If not, see <https://www.gnu.org/licenses/>.
+// ==============================================================================
+
+#include "knee_computer.h"
+
+namespace zlCompressor {
+    template<typename FloatType>
+    KneeComputer<FloatType>::KneeComputer(const KneeComputer<FloatType> &c) {
+        setThreshold(c.getThreshold());
+        setRatio(c.getRatio());
+        setKneeW(c.getKneeW());
+        setKneeD(c.getKneeD());
+        setKneeS(c.getKneeS());
+        setBound(c.getBound());
+    }
+
+    template<typename FloatType>
+    FloatType KneeComputer<FloatType>::eval(FloatType x) {
+        if (x <= threshold - kneeW) {
+            return x;
+        } else if (x >= threshold + kneeW) {
+            return juce::jlimit(x - bound.load(), x + bound.load(), x / ratio + (1 - 1 / ratio) * threshold);
+        } else {
+            try {
+                return juce::jlimit(x - bound.load(), x + bound.load(), cubic->operator()(x));
+            } catch (std::domain_error &e) {
+                return x;
+            } catch (...) {
+                return x;
+            }
+        }
+    }
+
+    template<typename FloatType>
+    FloatType KneeComputer<FloatType>::process(FloatType x) {
+        return juce::Decibels::decibelsToGain(eval(x) - x);
+    }
+
+    template<typename FloatType>
+    void KneeComputer<FloatType>::interpolate() {
+        std::array initialX{threshold.load() - kneeW.load(),
+                            threshold.load(),
+                            threshold.load() + kneeW.load()};
+        std::array initialY{threshold.load() - kneeW.load(),
+                            threshold.load() - kneeD.load() * FloatType(0.75) * kneeW.load() *
+                                               (FloatType(1) - FloatType(0.5) / ratio.load() - FloatType(0.5)),
+                            threshold.load() + kneeW.load() / ratio.load()};
+        std::array initialYX{FloatType(1),
+                             kneeS.load() + (FloatType(1) - kneeS.load()) / ratio.load(),
+                             FloatType(1) / ratio.load()};
+        cubic = std::make_unique<boost::math::interpolators::cubic_hermite<std::array<FloatType, 3>>>(
+                std::move(initialX),
+                std::move(initialY),
+                std::move(initialYX));
+    }
+
+    template
+    class KneeComputer<float>;
+
+    template
+    class KneeComputer<double>;
+} // KneeComputer
---
+++ source/dsp/compressor/computer/knee_computer.h
@@ -0,0 +1,81 @@
+// ==============================================================================
+// Copyright (C) 2023 - zsliu98
+// This file is part of ZLEComp
+//
+// ZLEComp is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// ZLEComp is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with ZLEComp. If not, see <https://www.gnu.org/licenses/>.
+// ==============================================================================
+
+#ifndef ZLECOMP_COMPUTER_H
+#define ZLECOMP_COMPUTER_H
+
+#include <boost/circular_buffer.hpp>
+#include <boost/math/interpolators/cubic_hermite.hpp>
+
+namespace zlCompressor {
+
+    template<typename FloatType>
+    class KneeComputer {
+    public:
+        KneeComputer() { interpolate(); }
+
+        KneeComputer(const KneeComputer<FloatType> &c);
+
+        FloatType eval(FloatType x);
+
+        FloatType process(FloatType x);
+
+        inline void setThreshold(FloatType v) {
+            threshold.store(v);
+            interpolate();
+        }
+
+        inline FloatType getThreshold() const { return threshold.load(); }
+
+        inline void setRatio(FloatType v) {
+            ratio.store(v);
+            interpolate();
+        }
+
+        inline FloatType getRatio() const { return ratio.load();}
+
+        inline void setKneeW(FloatType v) {
+            kneeW.store(v);
+            interpolate();
+        }
+
+        inline FloatType getKneeW() const {return kneeW.load();}
+
+        inline void setKneeD(FloatType v) {
+            kneeD.store(v);
+            interpolate();
+        }
+
+        inline FloatType getKneeD() const {return kneeD.load();}
+
+        inline void setKneeS(FloatType v) {
+            kneeS.store(v);
+            interpolate();
+        }
+
+        inline FloatType getKneeS() const {return kneeS.load();}
+
+        inline void setBound(FloatType v) {
+            bound.store(v);
+        }
+
+        inline FloatType getBound() const {return bound.load();}
+
+    private:
+        std::atomic<FloatType> threshold = FloatType(0), ratio = FloatType(1);
+        std::atomic<FloatType> kneeW = FloatType(0.0625), kneeD = FloatType(0.5), kneeS = FloatType(0.5);
+        std::atomic<FloatType> bound = FloatType(0);
+        std::unique_ptr<boost::math::interpolators::cubic_hermite<std::array<FloatType, 3>>> cubic;
+        void interpolate();
+    };
+
+} // KneeComputer
+
+#endif //ZLECOMP_COMPUTER_H
