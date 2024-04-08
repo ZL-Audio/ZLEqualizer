4e203708e8ff45327966f1a7f19ce298e11ec266
zsliu98 <zsliu98@gmail.com>
2024-04-08 12:50:54 -0400

    feat: add colour selector

---
+++ source/gui/colour_selector/colour_selector.cpp
@@ -0,0 +1,71 @@
+// Copyright (C) 2024 - zsliu98
+// This file is part of ZLEqualizer
+//
+// ZLEqualizer is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//
+// ZLEqualizer is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with ZLEqualizer. If not, see <https://www.gnu.org/licenses/>.
+
+#include "colour_selector.hpp"
+
+namespace zlInterface {
+    class SelectorBox final : public juce::Component {
+    public:
+        explicit SelectorBox(const int flags, zlInterface::UIBase &base)
+            : selector(flags), uiBase(base) {
+            selector.setColour(juce::ColourSelector::ColourIds::backgroundColourId, uiBase.getBackgroundColor());
+            addAndMakeVisible(selector);
+        }
+
+        ~SelectorBox() override {
+            setLookAndFeel(nullptr);
+        }
+
+        void resized() override {
+            auto bound = getLocalBounds().toFloat();
+            bound = bound.withSizeKeepingCentre(
+                bound.getWidth() - uiBase.getFontSize() * .5f,
+                bound.getHeight() - uiBase.getFontSize() * .5f);
+            selector.setBounds(bound.toNearestInt());
+        }
+
+        juce::ColourSelector &getSelector() { return selector; }
+
+    private:
+        juce::ColourSelector selector;
+        zlInterface::UIBase &uiBase;
+    };
+
+    ColourSelector::ColourSelector(zlInterface::UIBase &base, juce::Component &parent,
+        const float widthS, const float heightS)
+        : uiBase(base), laf(uiBase), parentC(parent),
+    selectorWidthS(widthS), selectorHeightS(heightS) {
+    }
+
+    void ColourSelector::paint(juce::Graphics &g) {
+        g.fillAll(colour);
+    }
+
+    void ColourSelector::mouseDown(const juce::MouseEvent &event) {
+        juce::ignoreUnused(event);
+        auto colourSelector = std::make_unique<SelectorBox>(
+            juce::ColourSelector::ColourSelectorOptions::showColourspace, uiBase);
+        colourSelector->getSelector().setCurrentColour(colour);
+        colourSelector->getSelector().addChangeListener(this);
+        colourSelector->setSize(juce::roundToInt(selectorWidthS * uiBase.getFontSize()),
+                                juce::roundToInt(selectorHeightS * uiBase.getFontSize()));
+        auto &box = juce::CallOutBox::launchAsynchronously(std::move(colourSelector),
+                                                           getBounds(), &parentC);
+        box.setLookAndFeel(&laf);
+        box.setArrowSize(0);
+        box.sendLookAndFeelChange();
+    }
+
+    void ColourSelector::changeListenerCallback(juce::ChangeBroadcaster *source) {
+        if (const auto *cs = dynamic_cast<juce::ColourSelector *>(source)) {
+            colour = cs->getCurrentColour();
+            repaint();
+        }
+    }
+} // zlInterface
---
+++ source/gui/colour_selector/colour_selector.hpp
@@ -0,0 +1,40 @@
+// Copyright (C) 2024 - zsliu98
+// This file is part of ZLEqualizer
+//
+// ZLEqualizer is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//
+// ZLEqualizer is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with ZLEqualizer. If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef COLOURSELECTOR_H
+#define COLOURSELECTOR_H
+
+#include <juce_gui_basics/juce_gui_basics.h>
+#include <juce_gui_extra/juce_gui_extra.h>
+#include "../interface_definitions.hpp"
+#include "../calloutbox/call_out_box_laf.hpp"
+
+namespace zlInterface {
+    class ColourSelector final : public juce::Component,
+                                 private juce::ChangeListener {
+    public:
+        explicit ColourSelector(zlInterface::UIBase &base, juce::Component &parent,
+            float widthS = 12.f, float heightS = 10.f);
+
+        void paint(juce::Graphics &g) override;
+
+        void mouseDown(const juce::MouseEvent &event) override;
+
+    private:
+        zlInterface::UIBase &uiBase;
+        zlInterface::CallOutBoxLAF laf;
+        juce::Component &parentC;
+        float selectorWidthS, selectorHeightS;
+        void changeListenerCallback(juce::ChangeBroadcaster *source) override;
+
+        juce::Colour colour = juce::Colours::red;
+    };
+} // zlInterface
+
+#endif //COLOURSELECTOR_H
