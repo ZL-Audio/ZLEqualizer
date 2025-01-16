5d93e1673083c4841e6aa8060845712750d1b8e6
zsliu98 <zsliu98@gmail.com>
2025-01-16 08:46:09 -0500

    fix(match analyzer panel): add explicit cast

--- source/panel/curve_panel/match_panel/match_analyzer_panel.hpp
+++ source/panel/curve_panel/match_panel/match_analyzer_panel.hpp
@@ -80,11 +80,11 @@ namespace zlPanel {
 
         void draggerValueChanged(zlInterface::Dragger *dragger) override;
 
-        void getIdxDBromPoint(const juce::Point<int> &p, size_t &idx, float &dB) {
+        void getIdxDBromPoint(const juce::Point<int> &p, size_t &idx, float &dB) const {
             const auto bound = getLocalBounds().toFloat();
             const auto idxInt = juce::roundToInt(250.f * (static_cast<float>(p.getX()) - bound.getX()) / bound.getWidth());
             idx = static_cast<size_t>(std::clamp(idxInt, 0, 250));
-            const auto yP = (p.getY() - bound.getY()) / bound.getHeight() - .5f;
+            const auto yP = (static_cast<float>(p.getY()) - bound.getY()) / bound.getHeight() - .5f;
             dB =  -maximumDB.load() * dBScale.load() * yP;
         }
     };
