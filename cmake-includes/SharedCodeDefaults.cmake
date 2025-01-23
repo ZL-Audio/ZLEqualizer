508261ec615ce41533065455563fb770048aeb59
zsliu98 <zsliu98@gmail.com>
2025-01-22 23:10:03 -0500

    ci: use clang provided by github runners

--- cmake-includes/SharedCodeDefaults.cmake
+++ cmake-includes/SharedCodeDefaults.cmake
@@ -1,12 +1,12 @@
 if (MSVC)
     # fast math and better simd support in RELEASE
     # https://learn.microsoft.com/en-us/cpp/build/reference/fp-specify-floating-point-behavior?view=msvc-170#fast
-    target_compile_options(SharedCode INTERFACE $<$<CONFIG:RELEASE>:/fp:fast>)
+    target_compile_options(SharedCode INTERFACE $<$<CONFIG:RELEASE>:/fp:precise>)
 else ()
     # See the implications here:
     # https://stackoverflow.com/q/45685487
-    target_compile_options(SharedCode INTERFACE $<$<CONFIG:RELEASE>:-Ofast>)
-    target_compile_options(SharedCode INTERFACE $<$<CONFIG:RelWithDebInfo>:-Ofast>)
+    target_compile_options(SharedCode INTERFACE $<$<CONFIG:RELEASE>:-O3 -fno-signed-zeros -freciprocal-math -ffp-contract=fast>)
+    target_compile_options(SharedCode INTERFACE $<$<CONFIG:RelWithDebInfo>:-O3 -fno-signed-zeros -freciprocal-math -ffp-contract=fast>)
 endif ()
 
 # Tell MSVC to properly report what c++ version is being used
