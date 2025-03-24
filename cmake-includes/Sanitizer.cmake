c6de605c4e913db31826c7cc5ee15765d0a32f01
zsliu98 <zsliu98@gmail.com>
2025-03-24 16:27:37 -0400

    chore: update sanitizers

--- cmake-includes/Sanitizer.cmake
+++ cmake-includes/Sanitizer.cmake
@@ -1,16 +1,23 @@
-# Enable Address Sanitizer
-if (DEFINED ENV{ADDRESS_SANITIZER_FLAG})
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=address")
-    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fsanitize=address")
-    add_compile_options("-fsanitize=address")
-    link_libraries("-fsanitize=address")
-    message("Enable Address Sanitizer")
+option(WITH_ADDRESS_SANITIZER "Enable Address Sanitizer" OFF)
+option(WITH_THREAD_SANITIZER "Enable Thread Sanitizer" OFF)
+
+message(STATUS "Sanitizers: ASan=${WITH_ADDRESS_SANITIZER} TSan=${WITH_THREAD_SANITIZER}")
+if (WITH_ADDRESS_SANITIZER)
+    if (MSVC)
+        add_compile_options(/fsanitize=address)
+    elseif (CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
+        # also enable UndefinedBehaviorSanitizer
+        # https://clang.llvm.org/docs/UndefinedBehaviorSanitizer.html
+        add_compile_options(-fsanitize=address,undefined -fno-omit-frame-pointer)
+        link_libraries(-fsanitize=address)
+    endif ()
+    message("Address Sanitizer enabled")
 endif ()
-# Enable Thread Sanitizer
-if (DEFINED ENV{THREAD_SANITIZER_FLAG})
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=thread")
-    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fsanitize=thread")
-    add_compile_options("-fsanitize=thread")
-    link_libraries("-fsanitize=thread")
-    message("Enable Thread Sanitizer")
+
+if (WITH_THREAD_SANITIZER)
+    if (CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
+        add_compile_options(-fsanitize=thread -g -fno-omit-frame-pointer)
+        link_libraries(-fsanitize=thread)
+        message("Thread Sanitizer enabled")
+    endif ()
 endif ()
