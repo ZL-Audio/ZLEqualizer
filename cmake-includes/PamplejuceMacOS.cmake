09d6a64021da35f633b2d42714bfd9953de2f36a
zsliu98 <zsliu98@gmail.com>
2026-04-14 12:19:54 -0400

    cmake: adjust macOS compile flags

--- cmake-includes/PamplejuceMacOS.cmake
+++ cmake-includes/PamplejuceMacOS.cmake
@@ -1,7 +1,12 @@
-
-# This must be set before the project() call
-# see: https://cmake.org/cmake/help/latest/variable/CMAKE_OSX_DEPLOYMENT_TARGET.html
-set(CMAKE_OSX_DEPLOYMENT_TARGET "10.13" CACHE STRING "Support macOS down to High Sierra")
-
-# By default we don't want Xcode schemes to be made for modules, etc
-set(CMAKE_XCODE_GENERATE_SCHEME OFF)
+if (CMAKE_HOST_APPLE)
+    if (CMAKE_OSX_ARCHITECTURES MATCHES "arm64")
+        set(CMAKE_OSX_DEPLOYMENT_TARGET "11" CACHE STRING "Support macOS down to macOS 11")
+        add_compile_options(-mcpu=apple-m1)
+        message(STATUS "arm64 macOS deployment target is macOS 11")
+    elseif (CMAKE_OSX_ARCHITECTURES MATCHES "x86_64")
+        set(CMAKE_OSX_DEPLOYMENT_TARGET "10.13" CACHE STRING "Support macOS down to macOS 10.13")
+        add_compile_options(-march=x86-64-v2)
+        message(STATUS "x86-64 macOS deployment target is macOS 10.13")
+    endif ()
+    set(CMAKE_XCODE_GENERATE_SCHEME OFF)
+endif ()
