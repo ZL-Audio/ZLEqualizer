d82d22eea9a5c309675039c2ecbef5cceedfa50c
zsliu98 <zsliu98@gmail.com>
2026-08-14 20:58:49 -0400

    fix(panel::fft_panel): fix incorrect gradient for the last bin

--- source/panel/curve_panel/fft_panel/fft_panel.cpp
+++ source/panel/curve_panel/fft_panel/fft_panel.cpp
@@ -340,7 +340,7 @@ namespace zlpanel {
             for (size_t i = 1; i < num_point_ - 1; ++i) {
                 gradient_minimizer.addColour(xs_[i] / width, coll_ps_[i]);
             }
-            gradient_minimizer.addColour(xs_[num_point_ - 1], coll_ps_[num_point_ - 1]);
+            gradient_minimizer.addColour(xs_[num_point_ - 1] / width, coll_ps_[num_point_ - 1]);
             gradient_minimizer.finish();
             gradient_.publish();
         }
